e6e2e84b8db5522cdc3272b9a13897bf0b2dfdc4
Juan Vargas <juandeveloper19@gmail.com>
2021-11-30 06:34:41 -0500

    DC heroes were implemented

--- src/components/dc/Dc.js
+++ src/components/dc/Dc.js
@@ -1,7 +1,15 @@
+import { HeroesList } from '../heroes/HeroesList';
+
+import '../../styles/dc.css';
+
 export const Dc = () => {
 	return (
-		<div>
-			<h2>Dc</h2>
+		<div className='dc'>
+			<div className='dc-content'>
+				<h2>Dc Comics</h2>
+
+				<HeroesList publisher='DC Comics' />
+			</div>
 		</div>
 	);
 };
