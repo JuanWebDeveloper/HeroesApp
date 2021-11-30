1c6cc612f6b8b3eeea427aded2fd9e3406c5cb57
Juan Vargas <juandeveloper19@gmail.com>
2021-11-30 04:10:07 -0500

    Home styles

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,11 +1,12 @@
 import { Search } from './Search';
 
+import '../../styles/home.css';
+
 export const HomePage = () => {
 	return (
 		<div className='home-page'>
 			<div className='home-page_content'>
 				<h1>Welcome to HeroesApp</h1>
-				<hr />
 
 				<Search />
 			</div>
