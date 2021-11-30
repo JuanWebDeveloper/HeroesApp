394ea4b20c91d0ceec7d4203e07b7f471ecbc996
Juan Vargas <juandeveloper19@gmail.com>
2021-11-30 07:29:49 -0500

    Hero information HTML

--- src/components/heroes/HeroInformation.js
+++ src/components/heroes/HeroInformation.js
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useParams, Navigate } from 'react-router-dom';
 
 import { getHeroById } from './helpers/getHeroById';
 
@@ -6,17 +6,40 @@ export const HeroInformation = () => {
 	const { id } = useParams();
 	const hero = getHeroById(id);
 
+	if (!hero) {
+		return <Navigate to='/' />;
+	}
+
+	const imagePath = `/assets/img/heroes/${id}.jpg`;
+
 	return (
-		<div>
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			{hero.superhero}
+		<div className='hero-information'>
+			<div className='hero-information_content'>
+				<div className='hero-image'>
+					<img src={imagePath} alt={hero.superhero} />
+				</div>
+
+				<div className='hero-description'>
+					<h2>{hero.superhero}</h2>
+
+					<ul>
+						<li>
+							<b>Alter ego:</b> {hero.alter_ego}
+						</li>
+						<li>
+							<b>Publisher:</b> {hero.publisher}
+						</li>
+						<li>
+							<b>First Appearance:</b> {hero.first_appearance}
+						</li>
+					</ul>
+
+					<h5>Characters</h5>
+					<p>{hero.characters}</p>
+
+					<button>Return</button>
+				</div>
+			</div>
 		</div>
 	);
 };
