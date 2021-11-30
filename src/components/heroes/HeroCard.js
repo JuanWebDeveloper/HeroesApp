0bda58b3096837e51648ee237cf874e335f9f07c
Juan Vargas <juandeveloper19@gmail.com>
2021-11-30 06:31:53 -0500

    The information on the cards is replaced

--- src/components/heroes/HeroCard.js
+++ src/components/heroes/HeroCard.js
@@ -1,17 +1,17 @@
 import { Link } from 'react-router-dom';
 
-export const HeroCard = () => {
-	const imagePath = `/assets/img/heroes/dc-batman.jpg`;
+export const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance, characters }) => {
+	const imagePath = `/assets/img/heroes/${id}.jpg`;
 
 	return (
 		<div className='hero-card'>
 			<div className='hero-img'>
-				<img src={imagePath} alt='' />
+				<img src={imagePath} alt={superhero} />
 			</div>
 			<div className='hero-data'>
-				<h3>Batman</h3>
-				<p>alter ego</p>
-				<p>first_appearance</p>
+				<h3>{superhero}</h3>
+				<p>{alter_ego}</p>
+				<p>{first_appearance}</p>
 				<Link to='/' className='more-information'>
 					More information
 				</Link>
