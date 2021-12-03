a6a26fa7bbc9a7ff65afb8553b68d00bdd83edf5
Juan Vargas <juandeveloper19@gmail.com>
2021-12-03 05:49:29 -0500

    Event to capture the value of the seeker

--- src/components/home/Search.js
+++ src/components/home/Search.js
@@ -1,13 +1,17 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 
 export const Search = () => {
+	const [searchValue, setSearchValue] = useState();
+
+	const handleChange = ({ target }) => setSearchValue(target.value);
+
 	return (
 		<Fragment>
 			<div className='search-hero'>
 				<h2>Look for a hero</h2>
 
 				<form>
-					<input type='text' name='search' placeholder='Search...' autoComplete='off' className='search' />
+					<input type='text' name='search' placeholder='Search...' autoComplete='off' className='search' value={searchValue} onChange={handleChange} />
 				</form>
 			</div>
 			<div className='search-results'>
